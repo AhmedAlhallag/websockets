ad1b2cf3f4d8894926254bbe4b470b0999c11917
AhmedAlhallag <aalhallag@gmail.com>
2021-07-18 09:51:26 +0400

    first commit

---
+++ chat-server.php
@@ -0,0 +1,16 @@
+<?php
+    require_once('vendor/autoload.php');
+    use MyApp\Chat ; 
+    use Ratchet\Server\IoServer;
+    use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+    $server = IoServer::factory(
+        new HttpServer(
+            new WsServer(
+                new Chat()
+            )
+        ),
+        8080
+    );
+
+    $server->run();
